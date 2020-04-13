d3e73004a55a7b5c79149c59f9ef14dcd4e491e1
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-13 17:27:05 -0400

    Refactor endpoints to replace db.select('*').from('accounts') with db('accounts')

--- api/accounts/accountsRouter.js
+++ api/accounts/accountsRouter.js
@@ -6,14 +6,14 @@ const router = express.Router();
 
 // GET accounts
 router.get("/", (req, res) => {
-    db.select("*").from("accounts")
+    db("accounts")
     .then(accounts => res.status(200).json({ data: accounts }))
     .catch(error => res.status(500).json({ error: error.message }))
 });
 
 // GET account by ID
 router.get("/:id", (req, res) => {
-    db.select("*").from("accounts").where("id", req.params.id)
+    db("accounts").where("id", req.params.id)
     .then(account => res.status(200).json({ data: account }))
     .catch(error => res.status(500).json({ error: error.message }))
 });
@@ -21,7 +21,7 @@ router.get("/:id", (req, res) => {
 // POST account
 router.post("/", (req, res) => {
     const accountData = req.body;
-    db.select("*").from("accounts").insert(accountData, "id")
+    db("accounts").insert(accountData, "id")
     .then(ids => {
         const id = ids[0];
         db.select("*").from("accounts").where({ id }).first()
@@ -31,7 +31,7 @@ router.post("/", (req, res) => {
 
 // DELETE account
 router.delete("/:id", (req, res) => {
-    db.select("*").from("accounts").where("id", req.params.id).del()
+    db("accounts").where("id", req.params.id).del()
     .then(num => res.status(200).json(num))
     .catch(error => res.status(500).json({ error: error.message }))
 });
@@ -39,7 +39,7 @@ router.delete("/:id", (req, res) => {
 // PUT account
 router.put("/:id", (req, res) => {
     const updatedAccount = req.body;
-    db.select("*").from("accounts").where("id", req.params.id).update(updatedAccount)
+    db("accounts").where("id", req.params.id).update(updatedAccount)
     .then(count => count > 0 ? res.status(200).json({ message: "Account update successful"}) : res.status(500).json({ message: "No account with that ID found"})
     )});
 
