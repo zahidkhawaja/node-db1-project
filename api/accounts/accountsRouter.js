e730a57a2c4c6fb11684bda98fff565ae5a11c1d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-13 17:15:16 -0400

    PUT endpoint

--- api/accounts/accountsRouter.js
+++ api/accounts/accountsRouter.js
@@ -4,18 +4,21 @@ const db = require("../../data/dbConfig");
 
 const router = express.Router();
 
+// GET accounts
 router.get("/", (req, res) => {
     db.select("*").from("accounts")
     .then(accounts => res.status(200).json({ data: accounts }))
     .catch(error => res.status(500).json({ error: error.message }))
 });
 
+// GET account by ID
 router.get("/:id", (req, res) => {
     db.select("*").from("accounts").where("id", req.params.id)
     .then(account => res.status(200).json({ data: account }))
     .catch(error => res.status(500).json({ error: error.message }))
 });
 
+// POST account
 router.post("/", (req, res) => {
     const accountData = req.body;
     db.select("*").from("accounts").insert(accountData, "id")
@@ -26,10 +29,18 @@ router.post("/", (req, res) => {
         .catch(error => res.status(500).json({ error: error.message }))
     })});
 
+// DELETE account
 router.delete("/:id", (req, res) => {
     db.select("*").from("accounts").where("id", req.params.id).del()
     .then(num => res.status(200).json(num))
     .catch(error => res.status(500).json({ error: error.message }))
 });
+
+// PUT account
+router.put("/:id", (req, res) => {
+    const updatedAccount = req.body;
+    db.select("*").from("accounts").where("id", req.params.id).update(updatedAccount)
+    .then(count => count > 0 ? res.status(200).json({ message: "Account update successful"}) : res.status(500).json({ message: "No account with that ID found"})
+    )});
 
 module.exports = router;
