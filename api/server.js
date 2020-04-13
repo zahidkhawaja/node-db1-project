01b1e3a1657a4a7753338c91c7437d54b8a73c7e
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-13 16:35:11 -0400

    Add accountsRouter.js and GET all accounts endpoint

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
+const AccountsRouter = require("./accounts/accountsRouter");
+
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", AccountsRouter);
+
+server.get("/", (req, res) => {
+    res.send(200).json({ api: "up"})
+})
 
 module.exports = server;
